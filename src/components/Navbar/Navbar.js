0c46d8f77e914f4fedbd5f9b1352147cfbb5365b
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2024-10-01 21:06:24 -0300

    se actualizo el navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { 
+  AppBar, 
+  Toolbar, 
+  Typography, 
+  Button, 
+  IconButton, 
+  Menu, 
+  MenuItem, 
+  Box,
+  useMediaQuery,
+  useTheme
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
 
-const Navbar = ({ setVista }) => (
-  <AppBar position="static">
-    <Toolbar>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        Sistema de Prontuarios
-      </Typography>
-      <Button color="inherit" onClick={() => setVista('inicio')}>Inicio</Button>
-      <Button color="inherit" onClick={() => setVista('nuevo')}>Nuevo</Button>
-      <Button color="inherit" onClick={() => setVista('lista')}>Lista</Button>
-    </Toolbar>
-  </AppBar>
-);
+const Navbar = ({ setVista }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleNavigation = (vista) => {
+    setVista(vista);
+    handleClose();
+  };
+
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="logo"
+          sx={{ mr: 2 }}
+        >
+          <DirectionsBoatIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Sistema de Prontuarios 
+        </Typography>
+        {isMobile ? (
+          <>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={() => handleNavigation('inicio')}>Inicio</MenuItem>
+              <MenuItem onClick={() => handleNavigation('nuevo')}>Nuevo</MenuItem>
+              <MenuItem onClick={() => handleNavigation('lista')}>Lista</MenuItem>
+            </Menu>
+          </>
+        ) : (
+          <Box>
+            <Button color="inherit" onClick={() => setVista('inicio')}>Inicio</Button>
+            <Button color="inherit" onClick={() => setVista('nuevo')}>Nuevo</Button>
+            <Button color="inherit" onClick={() => setVista('lista')}>Lista</Button>
+          </Box>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
