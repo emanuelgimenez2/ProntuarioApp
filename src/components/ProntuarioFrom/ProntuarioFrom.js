7593703f895828c47744feff39dfcd24be6f8281
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2024-10-03 20:20:40 -0300

    se continuo en el formulario del prontuario

--- src/components/ProntuarioFrom/ProntuarioFrom.js
+++ src/components/ProntuarioFrom/ProntuarioFrom.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { 
   Button, TextField, Card, CardContent, CardHeader, 
-  Container, Grid, Typography, FormControlLabel, Checkbox
-} from '@mui/material';
+  Container, Grid, Typography, FormControlLabel, Checkbox,
+  Select, MenuItem, InputLabel, FormControl
+} from '@mui/material'
 
 const ProntuarioMaritimo = () => {
   const [formData, setFormData] = useState({
@@ -103,6 +104,16 @@ const ProntuarioMaritimo = () => {
     event.preventDefault();
     console.log('Datos del prontuario:', formData);
     // Aquí iría la lógica para enviar los datos al servidor
+  };
+
+  const opcionesMorfologicas = {
+    cejas: ['Arqueadas', 'Onduladas', 'Rectas', 'Oblicua interna', 'Oblicua externa', 'Cejijuntas'],
+    parpados: ['Descubiertos', 'Inferiores abultados', 'Apretados exteriormente', 'Apretados interiormente', 'Exteriores levantados', 'Exteriores bajados', 'Estrabismo convergente ojo derecho', 'Estrabismo convergente en ambos ojos', 'Estrabismo divergente ojo derecho', 'Falta de ojo'],
+    nariz: ['Rectilinea horizontal', 'Ondulada horizontal', 'Concava horizontal', 'Convexa horizontal', 'Convexa recta horizontal', 'Concava elevada', 'Rectilinia baja', 'Ondulada baja', 'Concava baja'],
+    boca: ['Pequeña', 'Mediana', 'Grande'],
+    labios: ['Labios grueso', 'Labios finos', 'Superior grande', 'Inferior grande', 'Grande y ancha', 'Caidos', 'Pequeños', 'Asimetricos'],
+    menton: ['Fugitivo', 'Recto', 'Con hoyo', 'Saliente', 'Con hoyo prolongado', 'Con surco'],
+    orejas: ['Largas y grandes', 'Medio', 'Pequeñas o cortas', 'Salientes'],
   };
 
   return (
@@ -374,61 +385,31 @@ const ProntuarioMaritimo = () => {
         </Card>
 
         <Card sx={{ mb: 2 }}>
-          <CardHeader title="FILIACIÓN MORFOLÓGICA" />
-          <CardContent>
-            <Grid container spacing={2}>
-              {['frente', 'cejas', 'parpados', 'boca', 'labios', 'menton', 'orejas'].map((item) => (
-                <React.Fragment key={item}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      fullWidth
-                      name={item}
-                      label={item.charAt(0).toUpperCase() + item.slice(1)}
-                      value={formData[item]}
-                      onChange={handleInputChange}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      fullWidth
-                      name={`${item}Particularidades`}
-                      label={`Particularidades ${item}`}
-                      value={formData[`${item}Particularidades`]}
-                      onChange={handleInputChange}
-                    />
-                  </Grid>
-                </React.Fragment>
-              ))}
-              <Grid item xs={12} sm={4}>
-                <TextField
-                  fullWidth
-                  name="narizDorso"
-                  label="Nariz - Dorso"
-                  value={formData.narizDorso}
-                  onChange={handleInputChange}
-                />
+        <CardHeader title="FILIACIÓN MORFOLÓGICA" />
+        <CardContent>
+          <Grid container spacing={2}>
+            {Object.entries(opcionesMorfologicas).map(([campo, opciones]) => (
+              <Grid item xs={12} sm={6} key={campo}>
+                <FormControl fullWidth>
+                  <InputLabel id={`${campo}-label`}>{campo.charAt(0).toUpperCase() + campo.slice(1)}</InputLabel>
+                  <Select
+                    labelId={`${campo}-label`}
+                    id={campo}
+                    name={campo}
+                    value={formData[campo]}
+                    label={campo.charAt(0).toUpperCase() + campo.slice(1)}
+                    onChange={handleInputChange}
+                  >
+                    {opciones.map((opcion) => (
+                      <MenuItem key={opcion} value={opcion}>{opcion}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
               </Grid>
-              <Grid item xs={12} sm={4}>
-                <TextField
-                  fullWidth
-                  name="narizParticularidades"
-                  label="Nariz - Particularidades"
-                  value={formData.narizParticularidades}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <TextField
-                  fullWidth
-                  name="narizBase"
-                  label="Nariz - Base"
-                  value={formData.narizBase}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
+            ))}
+          </Grid>
+        </CardContent>
+      </Card>
 
         <Card sx={{ mb: 2 }}>
           <CardHeader title="PARENTESCOS" />
